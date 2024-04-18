35c69bc857395cf18fc3c1ce65e617a44300964d
tony blake <thanhle09t2bkdn@gmail.com>
2024-04-18 14:53:53 +0700

    role

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedSmallInteger('role');
+            $table->uuid('tenant_id')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
