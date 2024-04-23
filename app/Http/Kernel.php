bc430e0abed9e5cfd1b1f836a1fd4eea53925c49
tony blake <thanhle09t2bkdn@gmail.com>
2024-04-23 13:10:19 +0700

    tenant

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Stancl\Tenancy\Features\UniversalRoutes;
 use Stancl\Tenancy\Middleware\InitializeTenancyByRequestData;
 
 class Kernel extends HttpKernel
@@ -67,5 +68,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'roles' => \App\Http\Middleware\CheckRole::class,
         'tenant.header' => InitializeTenancyByRequestData::class,
+//        'universal' => UniversalRoutes::class,
     ];
 }
